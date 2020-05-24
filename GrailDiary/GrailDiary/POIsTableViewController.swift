f7120d18c97ab5b409da2f96da9e912abbe7a305
jeff kang <jeffkang@Jeffs-MacBook-Pro.local>
2020-05-24 06:46:03 -0500

    mvp complete

---
+++ GrailDiary/GrailDiary/POIsTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrailDiary
+//
+//  Created by Jeff Kang on 5/24/20.
+//  Copyright © 2020 Jeff Kang. All rights reserved.
+//
+
+import UIKit
+
+class POIsTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
